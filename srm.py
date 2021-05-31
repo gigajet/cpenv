00142181971b3dc5deb10f02bffc46528a232842
concua <gigajet08092000@gmail.com>
2021-05-31 13:53:19 +0700

    srm support / in #include "lib/baka.h"

--- srm.py
+++ srm.py
@@ -4,7 +4,7 @@ import re
 def srm (file):
     for line in file:
         line=line.rstrip('\n') #Do not delete indent & spacing
-        m = re.search('^#include "([.\w]+)"', line)
+        m = re.search('^#include "([.\w/]+)"', line)
         if m:
             with open(m.group(1),'r') as f:
                 srm(f)

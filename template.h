337cbb37b1db679fac9c64d8bdd67e8e0f437664
concua <gigajet08092000@gmail.com>
2021-05-31 13:41:06 +0700

    Problem class v2

--- template.h
+++ template.h
@@ -62,26 +62,50 @@ ll lcm (ll a, ll b) {return a/gcd(a,b)*b;}
 
 class Problem {
     public:
-        Problem() {}
-        virtual void solve()=0;
-    inline friend std::ostream& operator << (std::ostream &o, const Problem &p) {
-        return o << p.out.rdbuf();
+    virtual ~Problem() {} //Non-virtual interface idiom
+    virtual void main()=0; //every problem should implement this
+    Problem& solve() {main(); return *this;}
+
+    inline friend 
+    std::ostream& operator << (std::ostream &o, const Problem &p) 
+    {
+        return o << p.cout.rdbuf();
     }
-    inline friend std::istream& operator >> (std::istream &i, Problem &p) {
-        return i >> p.inp.rdbuf();
+
+    inline friend 
+    std::istream& operator >> (std::istream &i, Problem &p) 
+    {
+        return i >> p.cin.rdbuf();
+    }
+
+    inline friend 
+    Problem& operator << (Problem& receive, Problem& give) 
+    {
+        receive.cin << give.cout.rdbuf();
+        return receive;
+    }
+
+    inline friend
+    Problem& operator >> (Problem& give, Problem &receive)
+    {
+        give.cout >> receive.cin.rdbuf();
+        return give;
     }
 
     template <typename T> inline friend
-    Problem& operator << (Problem& p, const T& t) {
-        p.out << t; return p;
+    Problem& operator << (Problem& p, const T& t) 
+    {
+        p.cin << t; return p;
     }
 
     template <typename T> inline friend
-    Problem& operator >> (Problem& p, T& t) {
-        p.inp >> t; return p;
+    Problem& operator >> (Problem& p, T& t) 
+    {
+        p.cout >> t; return p;
     }
-    private:
-        std::stringstream inp, out;
+
+    protected:
+        std::stringstream cin, cout;
 };
 
 #endif
